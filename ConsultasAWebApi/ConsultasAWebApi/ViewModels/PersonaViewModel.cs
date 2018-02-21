d0da2374319587811346ee8a6937c43287eaeff0
Unknown <syero@iesnervion.es>
2018-02-21 20:53:12 +0100

    xq

---
+++ ConsultasAWebApi/ConsultasAWebApi/ViewModels/PersonaViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ConsultarAPI_CRUD_Personas_Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultasAWebApi.ViewModels
+{
+    class PersonaViewModel
+    {
+
+        private Persona persona;
+
+
+    }
+}
