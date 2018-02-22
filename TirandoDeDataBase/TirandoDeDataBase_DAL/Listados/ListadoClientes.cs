01c4bd55a07476cf33d6bf8e006f4896a545bee7
Unknown <syero@iesnervion.es>
2018-02-22 14:39:58 +0100

    s

---
+++ TirandoDeDataBase/TirandoDeDataBase_DAL/Listados/ListadoClientes.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TirandoDeDataBase_Entidades;
+
+namespace TirandoDeDataBase_DAL.Listados
+{
+    public class ListadoClientes
+    {
+        //Creamos el objeto de tipo conexion de mi conexion
+        Conexion miConexion = new Conexion();
+        //Creamos la sql Connection
+        SqlConnection conexion = new SqlConnection();
+        SqlCommand miComando = new SqlCommand();
+
+
+        /// <summary>
+        /// método que nos devuelve un listado de Clientes
+        /// </summary>
+        /// <returns>lista de personas</returns>
+        public List<Cliente> getListadoClientes()
+        {
+            //Listado de personas es para almacenar las personas que obtendremos cuando conenctemos con la bd
+            List<Cliente> listaClientes = new List<Cliente>();
+            SqlDataReader miLector;
+            Cliente cliente;
+
+            try
+            {
+
+                conexion = miConexion.getConnection();
+                //Creamos comandos
+                miComando.CommandText = "Select ID,Nombre,Apellidos,FechaNacimiento,Direccion,Telefono from Personas";
+                miComando.Connection = conexion;
+                miLector = miComando.ExecuteReader();
+
+                //Si hay lineas en el Lector
+                while (miLector.Read())
+                {
+                    cliente = new Cliente();
+                    cliente.codigo = (Int32)miLector["ID"];
+                    cliente.nombre = (String)miLector["Nombre"];
+                    cliente.numeroCuenta = (String)miLector["Apellidos"];
+                    person.direccion = (String)miLector["Direccion"];
+                    person.telefono = (String)miLector["Telefono"];
+
+                    listaPersonas.Add(person);
+                }//fin while
+            }
+            catch (SqlException sql) { throw sql; }
+
+
+            return listaPersonas;
+        }
+
+    }
+}
---
+++ TirandoDeDataBase/TirandoDeDataBase_Entidades/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TirandoDeDataBase_Entidades
+{
+     public class Cliente
+    {
+        public int codigo { get; set; }
+        public String nombre { get; set; }
+        public String direccion { get; set; }
+        public String numeroCuenta { get; set; }
+        public String telefono { get; set; }
+
+
+        public Cliente()
+        {
+            nombre = " ";
+            direccion = " ";
+            numeroCuenta = " ";
+            telefono = " ";
+        }
+
+        public Cliente(String telefono, String direccion, String numeroCuenta, String nombre)
+        {
+            this.nombre = nombre;
+            this.direccion = direccion;
+            this.numeroCuenta = numeroCuenta;
+            this.telefono = telefono;
+        }
+
+    }
+}
